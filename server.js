e4f9840982313505c8f4e9e7cd19f60b8f6bbd5b
jjnorris31 <jjnorris31@gmail.com>
2020-12-25 17:17:28 -0600

    feat: added typeDefs for User and Post models

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require('mongoose');
 require('dotenv').config({path: '.env'});
 const User = require('./models/User');
 const Post = require('./models/Post');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => console.log("DB Connected"))
 	.catch((err) => console.error(err))
-
-// schema -> type definitions
-const typeDefs = gql`
-	type Todo {
-		task: String
-		completed: Boolean
-	}
-	
-	type Query {
-		getTodos: [String]
-	}
-`;
 
 const server = new ApolloServer({
 	typeDefs,
