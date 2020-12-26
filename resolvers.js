2c8dddd0cee0a972b2b59aff1697dbdb1902e666
jjnorris31 <jjnorris31@gmail.com>
2020-12-26 13:24:43 -0600

    added getPosts query

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,27 @@
 module.exports = {
+	Query: {
+		getPosts: async (_, args, {Post}) => {
+			// population is the process of automatically replacing the specified
+			// path in the document with documents from other collections
+			return await Post.find({}).sort({createdAt: 'desc'}).populate({
+				path: 'createdBy',
+				model: 'User'
+			});
+		}
+	},
 	Mutation: {
+		addPost: async (
+			_,
+			{title, imageUrl, categories, description, creatorId},
+			{Post}) => {
+			return await new Post({
+				title,
+				imageUrl,
+				categories,
+				description,
+				createdBy: creatorId
+			}).save();
+		},
 		signUp: async (_, {username, email, password}, {User}) => {
 			const user = await User.findOne({username: username});
 			if (user) {
