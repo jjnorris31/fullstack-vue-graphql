1419bb8196cc4615e12716d9a5883881eacac4c7
jjnorris31 <jjnorris31@gmail.com>
2020-12-29 15:37:31 -0600

    feat: added carousel to the post's images

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading...</div>
-    <ul v-else
-        v-for="post in posts"
-        :key="post._id">
-      <li>{{post.title}}</li>
-    </ul>
+  <v-container v-if="posts">
+    <v-flex xs12>
+      <v-carousel v-bind="{'cycle': true}" interval="3000">
+        <v-carousel-item  v-for="post in posts"
+                          :src="post.imageUrl"
+                          :key="post._id">
+          <h1 id="carousel__id">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
@@ -30,6 +32,7 @@ export default {
           getPosts {
             _id
             title
+            imageUrl
           }
         }
       `,
@@ -47,3 +50,17 @@ export default {
   }
 }
 </script>
+
+<style>
+#carousel__id {
+  position: absolute;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0 ;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
