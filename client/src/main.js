03a1f8ed3e3de34d5be0dbfd5e00135fc8021f4b
jjnorris31 <jjnorris31@gmail.com>
2020-12-29 15:00:00 -0600

    feat: added first query with VueApollo and apollo-boost

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,24 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+// setup ApolloClient
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/'
+})
+
+const apolloProvider = new VueApollo({defaultClient});
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   vuetify,
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>{{post.title}}</li>
+    </ul>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import {gql} from 'apollo-boost';
 
 export default {
   name: 'Home',
   components: {
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+          }
+        }
+      `
+    }
   }
 }
 </script>
